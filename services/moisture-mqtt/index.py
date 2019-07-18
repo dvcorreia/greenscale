356703b28798491ff7c8021483beb54f7ae85cc4
dvcorreia <dv_correia@hotmail.com>
2019-07-18 19:05:56 +0100

    working version of mosquitto paho and docker receiving requests

---
+++ services/moisture-mqtt/index.py
@@ -0,0 +1,19 @@
+from mqtt import Client
+from mongoengine import connect
+import os
+
+print("Connecting to the database ...")
+connect("moistures", host="mongodb://" + os.environ.get('MONGO_USERNAME') +
+        ":" + os.environ.get('MONGO_PASSWORD') +
+        "@db-moisture:" + str(27017) + '/?authSource=admin')
+print("Connected to database!")
+
+if __name__ == '__main__':
+
+    print("Creating MQTT client ...")
+    client = Client(os.environ['HOST'],
+                    int(os.environ['PORT']),
+                    'test/1/')
+
+    print("Listening to channels!")
+    client.listen(forever=False)
--- services/moisture-mqtt/mqtt.py
+++ services/moisture-mqtt/mqtt.py
@@ -1,9 +1,11 @@
 import paho.mqtt.client as mqtt
 from time import sleep
+from schema import Moisture
+import json
 
 
-class Mqtt(object):
-    def __init__(self, host, port):
+class Client(object):
+    def __init__(self, host, port, channel):
         self.host = host
         self.port = port
 
@@ -11,28 +13,34 @@ class Mqtt(object):
 
         # Callbacks configuration
         self.mqtt.on_connect = self.onConnect
-        self.mqtt.on_message = self.onMessage
+        self.mqtt.on_message = lambda *_: print(
+            "Message received on a non handled channel")
+        # Channels callbacks
+        self.mqtt.message_callback_add(channel, self.onMessage)
 
         sleep(1)
 
         # Connect to the broker
         self.mqtt.connect(self.host, self.port)
 
-    def subscribe(self, channel):
+        sleep(1)
+
         self.mqtt.subscribe(channel)
-        self.mqtt.message_callback_add(channel, self.onMessage)
         print("Subscribed to channel " + channel)
 
     def publish(self, channel, message):
-        self.mqtt.publish(channel, message)
-        print('Pub on channel ' + channel + ': ' + message)
+        self.mqtt.publish(channel, json.dumps(message))
+        print('Pub on channel ' + channel + ': ' + json.dumps(message))
 
     def listen(self, forever=False):
         try:
             if forever:
+                print("Starting loop_forever thread ...", flush=True)
                 self.mqtt.loop_forever()
             else:
                 self.mqtt.loop_start()
+                while True:
+                    pass
         finally:
             print("\nClosing mqtt connect ...")
             self.mqtt.disconnect()
@@ -48,4 +56,13 @@ class Mqtt(object):
               self.host + " on port " + self.port)
 
     def onMessage(self, client, userdata, message):
-        print(str(message.payload.decode("utf-8")))
+        print(json.loads(str(message.payload.decode("utf-8"))), flush=True)
+
+        #m = Moisture()
+        #m.sensor = data['sensor']
+        #m.value = data['value']
+
+        # try:
+        #    m.save()
+        # except Exception as e:
+        #    print("couldn't save received measurement on DB:\n" + str(e))
